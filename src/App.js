4ece832080a15937ab16c19851ad210a9b8e70af
RobertGuglielmino <robert.gugliel@gmail.com>
2023-12-15 00:11:37 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+import photo from './blueshell.png';
+import { FaThumbsUp } from "react-icons/fa6";
+
+import {
+  useState,
+  useEffect
+} from "react"; 
+import { Center, VStack, Image, HStack, Input, IconButton } from '@chakra-ui/react'
+
 function App() {
+
+  const [coupon, setCoupon] = useState("");
+  const [couponWorks, setCouponWorks] = useState(false);
+
+  const goodCoupon = "CONQUERYOURNEWYEARSRESOLUTIONNOMATTERWHAT";
+
+  
+  useEffect(() => {
+    document.title="CONQUER!!!";
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Center>
+        <VStack w={1000}>
+          {couponWorks && <Image padding={10} src={photo}/>}
+          <HStack>
+            <Input
+              w={500}
+              placeholder="Coupon Code Here"
+              padding="5px"
+              value={coupon}
+              onChange={e => setCoupon(e.target.value)}
+              />
+            <IconButton
+              onClick={buttonClick}
+              colorScheme="green"
+              icon={<FaThumbsUp />}
+              />
+          </HStack>
+        </VStack>
+      </Center>
     </div>
   );
+
+  function buttonClick() {
+    if (coupon.toUpperCase() === goodCoupon) setCouponWorks(true);
+    
+  }
+
 }
 
 export default App;
