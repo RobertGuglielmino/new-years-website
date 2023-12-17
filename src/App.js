0bf3784874845bb69fa809837d59dfebb08fd733
RobertGuglielmino <robert.gugliel@gmail.com>
2023-12-16 22:20:03 -0500

    yes!!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-import photo from './blueshell.png';
+import useSound from 'use-sound';
+
+import pointPhoto from './point-to-code.jpg';
+import successPic from './success-pic.jpg';
+import successCat from './success-cat.jpg';
+import conquer from './conquer.mp3';
 import { FaThumbsUp } from "react-icons/fa6";
 
 import {
   useState,
   useEffect
 } from "react"; 
-import { Center, VStack, Image, HStack, Input, IconButton } from '@chakra-ui/react'
+import { Center, VStack, Image, HStack, Input, IconButton, Heading } from '@chakra-ui/react'
 
 function App() {
 
   const [coupon, setCoupon] = useState("");
   const [couponWorks, setCouponWorks] = useState(false);
+  const [havePlayed, setHavePlayed] = useState(false);
+  const [playSound] = useSound(conquer);
 
   const goodCoupon = "CONQUERYOURNEWYEARSRESOLUTIONNOMATTERWHAT";
 
+  const pic = (Math.random() < 0.1) ? successCat : successPic;
   
   useEffect(() => {
     document.title="CONQUER!!!";
@@ -26,7 +34,8 @@ function App() {
     <div className="App">
       <Center>
         <VStack w={1000}>
-          {couponWorks && <Image padding={10} src={photo}/>}
+          <Heading>CONQUER YOUR NEW YEARS RESOLUTION BELOW!!</Heading>
+          {!couponWorks ? <Image boxSize='700px' padding={10} src={pointPhoto}/> : <Image boxSize='700px' padding={10} src={pic}/>}
           <HStack>
             <Input
               w={500}
@@ -47,8 +56,13 @@ function App() {
   );
 
   function buttonClick() {
-    if (coupon.toUpperCase() === goodCoupon) setCouponWorks(true);
-    
+    if (coupon.toUpperCase() === goodCoupon) {
+      setCouponWorks(true);
+      if (!havePlayed) {
+        setHavePlayed(true);
+        playSound();
+      }
+    }
   }
 
 }
